1f666f56274c6d138ad21fbadfdc882317c5faa0
Raj Mehta <mehtaraj1234.rm@gmail.com>
2024-04-06 11:31:27 -0400

    Made changes for Confluent

--- consumer.py
+++ consumer.py
@@ -1,12 +1,38 @@
+import streamlit as st
 from kafka import KafkaConsumer
 import json
+from dotenv import load_dotenv
+import os
+from confluent_kafka import admin
 
-# Change This
-broker_addresses = ['RajTest123-kafka.REGION.gcp.confluent.cloud:9092']
+# Load environment variables from .env file
+load_dotenv()
+
+bootstrap = os.getenv('CONFLUENT_CLUSTER')
+api_key = os.getenv('CONFLUENT_KEY')
+api_secret = os.getenv('CONFLUENT_SECRET')
+
+# Connect to the Confluent Kafka cluster
+props = {
+    'bootstrap.servers': bootstrap,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': api_key,
+    'sasl.password': api_secret
+}
+
+# Create a Kafka admin client
+admin_client = admin.AdminClient(props)
+
+# Get the cluster metadata
+cluster_metadata = admin_client.list_topics(timeout=10)
+
+# # Extract the broker addresses
+# broker_addresses = [node.split(':')[0] for broker in cluster_metadata.brokers]
 
 # Create a Kafka consumer
 consumer = KafkaConsumer('chat-topic',
-                         bootstrap_servers=broker_addresses,
+                         bootstrap_servers=bootstrap,
                          auto_offset_reset='earliest',
                          enable_auto_commit=True,
                          value_deserializer=lambda m: json.loads(m.decode('utf-8')))
--- producer.py
+++ producer.py
@@ -1,14 +1,38 @@
 import streamlit as st
 from kafka import KafkaProducer
 import json
+from dotenv import load_dotenv
+import os
+from confluent_kafka import admin
 
-#Replace this from GCP 
-broker_addresses = ['RajTest123-kafka.REGION.gcp.confluent.cloud:9092']
+# Load environment variables from .env file
+load_dotenv()
+
+bootstrap = os.getenv('CONFLUENT_CLUSTER')
+api_key = os.getenv('CONFLUENT_KEY')
+api_secret = os.getenv('CONFLUENT_SECRET')
+
+# Connect to the Confluent Kafka cluster
+props = {
+    'bootstrap.servers': bootstrap,
+    'security.protocol': 'SASL_SSL',
+    'sasl.mechanism': 'PLAIN',
+    'sasl.username': api_key,
+    'sasl.password': api_secret
+}
+
+# Create a Kafka admin client
+admin_client = admin.AdminClient(props)
+
+# Get the cluster metadata
+cluster_metadata = admin_client.list_topics(timeout=10)
+
+# Extract the broker addresses
+# broker_addresses = [node.split(':')[0] for broker in cluster_metadata.brokers]
 
 # Create a Kafka producer
-producer = KafkaProducer(bootstrap_servers=broker_addresses,
+producer = KafkaProducer(bootstrap_servers=bootstrap,
                          value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
 
 # Streamlit app
 def main():
